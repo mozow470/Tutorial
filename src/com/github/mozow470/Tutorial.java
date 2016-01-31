a83f5e2f61731d40e76535cd984543fdc2375d18
Mozo <mozow4780@gmail.com>
2016-01-31 21:56:02 +0900

    Create Tutorial.java

---
+++ src/com/github/mozow470/Tutorial.java
@@ -0,0 +1,27 @@
+package com.github.mozow470.tutorial;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Tutorial extends JavaPlugin implements Listener{
+	public void onEnable()
+	{
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	public void onDisable()
+	{
+		
+	}
+	@EventHandler
+	public void onJoinEvent(PlayerJoinEvent e)
+	{
+		Player player=e.getPlayer();
+		e.setJoinMessage("Hello "+player.getName().toString());
+		
+	}
+	
+	
+}
